2d08773f21982f67dc3f81692ca5c82b989a0eba
flpegomes <felipegomesfurtado@gmail.com>
2022-06-27 21:58:44 -0300

    teste login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { MenuIcon } from "../assets/MenuIcon";
 
+
 export function Header() {
     return (
         <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
