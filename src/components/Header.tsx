8d7f71fb9e54f0d0d0fd9fff0e02a5a883b57c47
GGomesz <fernandogomessanho@gmail.com>
2022-06-27 21:47:46 -0300

    app

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { MenuIcon } from "../assets/MenuIcon";
+
+export function Header() {
+    return (
+        <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
+            <MenuIcon />
+        </header>
+    )
+}
